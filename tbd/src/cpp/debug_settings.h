9a2ebdf9aeadad98cd28193b62514940e68b6feb
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2024-04-08 06:52:50 -0400

    broke debug parts into multiple flags

---
+++ tbd/src/cpp/debug_settings.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// if in debug mode then set everything, otherwise uncomment turning things off if trying to debug specific things
+#define DEBUG_FUEL_VARIABLE
+#define DEBUG_FWI_WEATHER
+#define DEBUG_GRIDS
+#define DEBUG_PROBAILITY
+#define DEBUG_SIMULATION
+#define DEBUG_STATISTICS
+#define DEBUG_WEATHER
+
+#ifdef NDEBUG
+
+#undef DEBUG_FUEL_VARIABLE
+#undef DEBUG_FWI_WEATHER
+#undef DEBUG_GRIDS
+#undef DEBUG_PROBAILITY
+#undef DEBUG_SIMULATION
+#undef DEBUG_STATISTICS
+#undef DEBUG_WEATHER
+
+#endif
+
+#if defined(NDEBUG) || defined(DEBUG_FUEL_VARIABLE) || defined(DEBUG_FWI_WEATHER) || defined(DEBUG_GRIDS) || defined(DEBUG_PROBABILITY) || defined(DEBUG_FWI_WEATHER) || defined(DEBUG_FWI_WEATHER) || defined(DEBUG_FWI_WEATHER)
+#define DEBUG_ANY
+#endif
+
+namespace tbd::debug
+{
+void show_debug_settings();
+}
