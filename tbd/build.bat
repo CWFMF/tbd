bcbe08ee3f3e5ed0d44817847aa5bb7b5ee143dd
Jordan Evens <jordan.evens@canada.ca>
2025-02-26 15:28:17 -0500

    read VCPKG_ROOT from env if not set

--- tbd/build.bat
+++ tbd/build.bat
@@ -1,6 +1,5 @@
 set VARIANT=%1
 if "%VARIANT%" == "" set VARIANT=Release
-set TOOLCHAIN=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake
 
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=%VARIANT% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN%" -S"." -B"./build"
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=%VARIANT%  -S"." -B"./build"
 cmake --build "./build" --config %VARIANT% --target tbd -j 50 --
